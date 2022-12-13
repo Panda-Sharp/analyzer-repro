816dd5fcbc0057d81000e4f3a30de62c91722832
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2022-12-13 10:08:47 +0100

    Add project files.

---
+++ WinUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
+
+namespace WinUI.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private int a = 0;
+
+        public MainViewModel()
+        {
+            GetDelayCommand = new AsyncRelayCommand(GetDelay);
+        }
+
+        public IAsyncRelayCommand GetDelayCommand { get; }
+
+        public async Task GetDelay()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ WinUI/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Windows.UI.Xaml.Controls;
+using WinUI.ViewModels;
+
+namespace WinUI.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            DataContext = App.Current.Services.GetService<MainViewModel>();
+        }
+
+        private MainViewModel ViewModel => (MainViewModel)DataContext;
+    }
+}
